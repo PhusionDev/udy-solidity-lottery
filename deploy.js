396a49296b58096740ff4b59d1e72ef4a395ffaf
Jared Hodgkins <phusiondev@gmail.com>
2021-12-04 21:50:17 -0500

    Initial commit from boilerplate - added process.env

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const SEED_PHRASE = process.env.ETH_DEV_SEED_PHRASE;
+const INFURA_ID = process.env.INFURA_ID;
+const ENDPOINT =
+  "https://ropsten.infura.io/v3/62e143ca4e444cc4a72f8f3d4de6e6ba";
+
+const provider = new HDWalletProvider(SEED_PHRASE, ENDPOINT);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
