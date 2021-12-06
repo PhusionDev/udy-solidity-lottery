ba11105ff5042c196d5671f3d5500a2ae98c9fcf
Jared Hodgkins <phusiondev@gmail.com>
2021-12-06 13:07:56 -0500

    Changed ENDPOINT const to point to process.env.INFURA_ENDPOINT

--- deploy.js
+++ deploy.js
@@ -1,10 +1,9 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
 const SEED_PHRASE = process.env.ETH_DEV_SEED_PHRASE;
-const ENDPOINT =
-  "https://ropsten.infura.io/v3/62e143ca4e444cc4a72f8f3d4de6e6ba";
+const ENDPOINT = process.env.INFURA_ENDPOINT;
 
 const provider = new HDWalletProvider(SEED_PHRASE, ENDPOINT);
 const web3 = new Web3(provider);
@@ -12,14 +11,14 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
+    .send({ gas: '1000000', from: accounts[0] });
 
   console.log(interface);
-  console.log("Contract deployed to", result.options.address);
+  console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
 deploy();
